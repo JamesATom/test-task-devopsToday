4067a6c3a76e00d74a469be843fa60ef6778a666
JamesATom <abduhakimovabdulaziz7@gmail.com>
2025-02-12 15:30:18 +0500

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 // app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './modules/v1/user/user.module';
+import { AuthModule } from './modules/v1/auth/auth.module';
+import { PostModule } from './modules/v1/post/post.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from './config/config.module';
+import { UserModule } from './modules/v2/user/user.module';
 
 @Module({
     imports: [
@@ -8,6 +14,11 @@ import { ConfigModule } from '@nestjs/config';
             isGlobal: true,
             envFilePath: '.env',
         }),
+		UserModule,
+		AuthModule,
+		PostModule,
+		CommonModule,
+		ConfigModule,
 	],
     controllers: [],
     providers: [],
